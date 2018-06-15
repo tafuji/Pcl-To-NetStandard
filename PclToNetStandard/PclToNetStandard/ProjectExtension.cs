f4271740a570262183256a27c6f1f92d96530a29
Takeshi Fujimoto <tfujimoto1223@gmail.com>
2018-06-15 18:28:50 +0900

    Add a extension.

---
+++ PclToNetStandard/PclToNetStandard/ProjectExtension.cs
@@ -0,0 +1,28 @@
+﻿using EnvDTE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PclToNetStandard
+{
+    internal static class ProjectExtension
+    {
+        /// <summary>
+        /// Package config file name.
+        /// </summary>
+        private const string PackageConfigFileName = "packages.config";
+
+        /// <summary>
+        /// Package config file path of the project.
+        /// </summary>
+        /// <param name="project">project</param>
+        /// <returns></returns>
+        public static string GetPackageConfigFilePath(this Project project)
+        {
+            var rootPath = System.IO.Path.GetDirectoryName(project.FileName);
+            return System.IO.Path.Combine(rootPath, PackageConfigFileName);
+        }
+    }
+}
